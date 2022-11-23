e7d33ce9064dba0640db44ea052437e465126fca
Xpl0itU <samuelcarballogarcia8@gmail.com>
2022-11-23 18:56:22 +0100

    Try to fix little endian issues

--- src/downloader.c
+++ src/downloader.c
@@ -44,18 +44,6 @@ static size_t previousDownloadedSize = 0;
 static char totalSize[255];
 static bool *queueCancelled;
 
-static inline uint16_t bswap_16(uint16_t value) {
-    return (uint16_t) ((0x00FF & (value >> 8)) | (0xFF00 & (value << 8)));
-}
-
-static inline uint32_t bswap_32(uint32_t __x) {
-    return __x >> 24 | __x >> 8 & 0xff00 | __x << 8 & 0xff0000 | __x << 24;
-}
-
-static inline uint64_t bswap_64(uint64_t x) {
-    return (((x & 0xff00000000000000ull) >> 56) | ((x & 0x00ff000000000000ull) >> 40) | ((x & 0x0000ff0000000000ull) >> 24) | ((x & 0x000000ff00000000ull) >> 8) | ((x & 0x00000000ff000000ull) << 8) | ((x & 0x0000000000ff0000ull) << 24) | ((x & 0x000000000000ff00ull) << 40) | ((x & 0x00000000000000ffull) << 56));
-}
-
 static char *readable_fs(double size, char *buf) {
     int i = 0;
     const char *units[] = {"B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};

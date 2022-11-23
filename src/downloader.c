5216661a3fcae217c8dbf3150e56c7164e61a941
Xpl0itU <samuelcarballogarcia8@gmail.com>
2022-11-23 13:38:53 +0100

    Cleanup

--- src/downloader.c
+++ src/downloader.c
@@ -20,11 +20,6 @@
 #include <curl/curl.h>
 #include <gtk/gtk.h>
 
-struct PathFileStruct {
-    char *file_path;
-    FILE *file_pointer;
-};
-
 struct MemoryStruct {
     uint8_t *memory;
     size_t size;
@@ -54,7 +49,7 @@ static inline uint32_t bswap_32(uint32_t __x) {
     return __x >> 24 | __x >> 8 & 0xff00 | __x << 8 & 0xff0000 | __x << 24;
 }
 
-inline uint64_t bswap_64(uint64_t x) {
+static inline uint64_t bswap_64(uint64_t x) {
     return (((x & 0xff00000000000000ull) >> 56) | ((x & 0x00ff000000000000ull) >> 40) | ((x & 0x0000ff0000000000ull) >> 24) | ((x & 0x000000ff00000000ull) >> 8) | ((x & 0x00000000ff000000ull) << 8) | ((x & 0x0000000000ff0000ull) << 24) | ((x & 0x000000000000ff00ull) << 40) | ((x & 0x00000000000000ffull) << 56));
 }
 

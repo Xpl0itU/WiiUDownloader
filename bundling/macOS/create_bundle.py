20346c1dbbd68ec21ae75a9366ba3fc36099a933
Xpl0itU <samuelcarballogarcia8@gmail.com>
2023-01-01 19:54:03 +0100

    Create a DMG with WiiUDownloader.app

---
+++ bundling/macOS/create_bundle.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+import subprocess
+
+# Set the paths to the executable and Info.plist
+executable_path = 'build/WiiUDownloader'
+info_plist_path = 'bundling/macOS/Info.plist'
+
+# Set the path to the .app bundle
+app_bundle_path = 'out/WiiUDownloader.app'
+
+# Create the .app bundle
+os.makedirs(os.path.join(app_bundle_path, 'Contents', 'MacOS'))
+shutil.copy(info_plist_path, os.path.join(app_bundle_path, 'Contents', 'Info.plist'))
+shutil.copy(executable_path, os.path.join(app_bundle_path, 'Contents', 'MacOS', 'WiiUDownloader'))
+
+# Run dylibbundler
+os.system(f"dylibbundler -od -b -x {os.path.join(app_bundle_path, 'Contents', 'MacOS', 'WiiUDownloader')} -d {os.path.join(app_bundle_path, 'Contents', 'MacOS', 'lib')} -p @executable_path/lib")

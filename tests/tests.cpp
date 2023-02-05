dace254435e7c74f70fddba1db6dff0f992dcc4f
Xpl0itU <samuelcarballogarcia8@gmail.com>
2023-02-05 23:44:34 +0100

    Rename tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,14 +1,14 @@
 #include <catch2/catch_test_macros.hpp>
 #include <downloader.h>
 
-TEST_CASE("Title downloads work", "[download]") {
+TEST_CASE("Title downloads", "[download]") {
     setSelectedDir(".");
     bool cancelQueue = false;
     int downloadValue = downloadTitle("0005001010004000", "OSv0", false, &cancelQueue, false, false);
     REQUIRE(downloadValue == 0);
 }
 
-TEST_CASE("Title decryption works", "[download]") {
+TEST_CASE("Title decryption", "[decryption]") {
     setSelectedDir(".");
     bool cancelQueue = false;
     int downloadValue = downloadTitle("0005001010004000", "OSv0", true, &cancelQueue, false, false);

380c8fea50868959b61a0d1e4ab73031d1a9317e
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2024-01-20 14:15:52 +0100

    Try to fix LDFLAGS order

--- pkg/aria2go/libaria2.go
+++ pkg/aria2go/libaria2.go
@@ -5,7 +5,7 @@ package aria2go
 /*
  #cgo CXXFLAGS: -std=c++11 -I./aria2-lib/include -Werror -Wall
  #cgo LDFLAGS: -L./aria2-lib/lib
- #cgo LDFLAGS: -lcrypto -lssl -lcares -laria2
+ #cgo LDFLAGS: -laria2 -lcares -lssl -lcrypto
  #cgo darwin LDFLAGS: -framework Security
  #include <stdlib.h>
  #include "aria2_c.h"

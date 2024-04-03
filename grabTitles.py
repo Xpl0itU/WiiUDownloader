e3334c9d0ee71ea5e0d26063bc0f147033905ddb
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2024-04-03 18:38:09 +0200

    Fix database fetching

--- grabTitles.py
+++ grabTitles.py
@@ -1,24 +1,27 @@
 #!/bin/env python
 
 import os
-import urllib.request
-import ssl
+import requests
 
 # Don't edit below this line
+
 
 def checkAndDeleteFile(file):
     if os.path.exists(file):
         print(f"Deleting {file}")
         os.remove(file)
 
-# Disable certificate verification
-ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
 
-opener = urllib.request.build_opener(urllib.request.HTTPSHandler(context=ssl_context))
-opener.addheaders = [("User-agent", "NUSspliBuilder/2.2"), ("Accept-Encoding", "gzip, deflate, br")]
-urllib.request.install_opener(opener)
+def downloadFile(url, file, headers={}):
+    print(f"Downloading {file}")
+    with requests.get(url, headers=headers) as r:
+        with open(file, "wb") as f:
+            f.write(r.content)
+
 
 checkAndDeleteFile("db.go")
-urllib.request.urlretrieve("https://napi.v10lator.de/db?t=go", "db.go")
+downloadFile(
+    "https://napi.v10lator.de/db?t=go",
+    "db.go",
+    {"User-Agent": "NUSspliBuilder/2.2", "Accept-Encoding": "br, gzip, deflate"},
+)

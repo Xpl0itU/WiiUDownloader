5066e2ec582242c6ba6a49aba43a40330b4d5899
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2026-01-25 16:20:10 +0100

    Redesign initial setup assistant

--- cmd/WiiUDownloader/initialSetupAssistant.go
+++ cmd/WiiUDownloader/initialSetupAssistant.go
@@ -24,20 +24,74 @@ func NewInitialSetupAssistantWindow(config *Config) (*InitialSetupAssistantWindo
 		os.Exit(0) // Hacky way to close the program
 	})
 	assistant.SetTitle("WiiUDownloader - Initial Setup")
-	assistant.SetDefaultSize(500, 400)
+	assistant.SetDefaultSize(600, 500)
+	assistant.SetPosition(gtk.WIN_POS_CENTER)
+	assistant.SetKeepAbove(true)
 
 	page1, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
 	if err != nil {
 		return nil, err
 	}
-	page1.SetBorderWidth(10)
+	page1.SetBorderWidth(24)
+	page1.SetSpacing(16)
 	assistant.AppendPage(page1)
 
-	page1Label, err := gtk.LabelNew("Welcome to WiiUDownloader! This assistant will help you set up the program.\nNote: This is a one-time setup, you can always change these settings from the program interface itself.")
+	page1Label, err := gtk.LabelNew("")
 	if err != nil {
 		return nil, err
 	}
-	page1.PackStart(page1Label, true, true, 0)
+	page1Label.SetMarkup("<span font='18' weight='bold'>Welcome to WiiUDownloader</span>")
+	page1Label.SetHAlign(gtk.ALIGN_START)
+	page1.PackStart(page1Label, false, false, 0)
+
+	page1SubLabel, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	page1SubLabel.SetMarkup("<span font='11' alpha='85%'>This setup wizard will guide you through the initial configuration in just a few steps. You can modify these settings anytime later in the preferences.</span>")
+	page1SubLabel.SetLineWrap(true)
+	page1SubLabel.SetHAlign(gtk.ALIGN_START)
+	page1.PackStart(page1SubLabel, false, false, 0)
+
+	// Spacing
+	spacer, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	page1.PackStart(spacer, true, true, 0)
+
+	// Info items
+	infoBox, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	infoBox.SetSpacing(8)
+
+	info1, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	info1.SetMarkup("<span font='10' alpha='80%'>▸ Select your preferred game regions</span>")
+	info1.SetHAlign(gtk.ALIGN_START)
+	infoBox.PackStart(info1, false, false, 0)
+
+	info2, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	info2.SetMarkup("<span font='10' alpha='80%'>▸ Choose target platforms (emulator and/or console)</span>")
+	info2.SetHAlign(gtk.ALIGN_START)
+	infoBox.PackStart(info2, false, false, 0)
+
+	info3, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	info3.SetMarkup("<span font='10' alpha='80%'>▸ Review and confirm your configuration</span>")
+	info3.SetHAlign(gtk.ALIGN_START)
+	infoBox.PackStart(info3, false, false, 0)
+
+	page1.PackStart(infoBox, false, false, 8)
 
 	// Skip button
 	skipButton, err := gtk.ButtonNewWithLabel("Skip")
@@ -60,27 +114,61 @@ func NewInitialSetupAssistantWindow(config *Config) (*InitialSetupAssistantWindo
 	if err != nil {
 		return nil, err
 	}
-	page2.SetBorderWidth(10)
+	page2.SetBorderWidth(24)
+	page2.SetSpacing(12)
 	assistant.AppendPage(page2)
 
-	page2Label, err := gtk.LabelNew("Please select your region(s):")
+	page2Label, err := gtk.LabelNew("")
 	if err != nil {
 		return nil, err
 	}
-	page2.PackStart(page2Label, true, true, 0)
+	page2Label.SetMarkup("<span font='14' weight='bold'>Which regions do you want to download from?</span>")
+	page2Label.SetHAlign(gtk.ALIGN_START)
+	page2.PackStart(page2Label, false, false, 0)
 
-	regionBox, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
+	page2Desc, err := gtk.LabelNew("")
 	if err != nil {
 		return nil, err
 	}
-	page2.PackStart(regionBox, true, true, 0)
+	page2Desc.SetMarkup("<span font='11' alpha='80%'>Select one or more regions to enable downloading games from their respective game libraries.</span>")
+	page2Desc.SetHAlign(gtk.ALIGN_START)
+	page2Desc.SetLineWrap(true)
+	page2.PackStart(page2Desc, false, false, 0)
+
+	regionList, err := gtk.ListBoxNew()
+	if err != nil {
+		return nil, err
+	}
+	regionList.SetSelectionMode(gtk.SELECTION_NONE)
+	regionList.SetActivateOnSingleClick(false)
+	page2.PackStart(regionList, true, true, 8)
 
 	selectedRegionCheckboxes := uint8(0)
 
-	europeCheck, err := gtk.CheckButtonNewWithLabel("Europe")
+	// Europe
+	europeRow, err := gtk.ListBoxRowNew()
 	if err != nil {
 		return nil, err
 	}
+	europeRow.SetSelectable(false)
+	europeContainer, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	europeContainer.SetMarginStart(16)
+	europeContainer.SetMarginEnd(16)
+	europeContainer.SetMarginTop(12)
+	europeContainer.SetMarginBottom(12)
+	europeContainer.SetSpacing(12)
+	europeRow.Add(europeContainer)
+
+	europeCheck, err := gtk.CheckButtonNewWithLabel("")
+	if err != nil {
+		return nil, err
+	}
+	europeCheck.SetActive(true)
+	selectedRegionCheckboxes++
+	europeCheck.SetVAlign(gtk.ALIGN_CENTER)
 	europeCheck.Connect("toggled", func() {
 		if europeCheck.GetActive() {
 			selectedRegionCheckboxes++
@@ -89,12 +177,42 @@ func NewInitialSetupAssistantWindow(config *Config) (*InitialSetupAssistantWindo
 		}
 		assistant.SetPageComplete(page2, selectedRegionCheckboxes > 0)
 	})
-	regionBox.PackStart(europeCheck, true, true, 0)
+	europeContainer.PackStart(europeCheck, false, false, 0)
 
-	usaCheck, err := gtk.CheckButtonNewWithLabel("USA")
+	europeLabel, err := gtk.LabelNew("")
 	if err != nil {
 		return nil, err
 	}
+	europeLabel.SetMarkup("<span font='12' weight='600'>Europe</span>")
+	europeLabel.SetHAlign(gtk.ALIGN_START)
+	europeLabel.SetVAlign(gtk.ALIGN_CENTER)
+	europeContainer.PackStart(europeLabel, true, true, 0)
+	regionList.Add(europeRow)
+
+	// USA
+	usaRow, err := gtk.ListBoxRowNew()
+	if err != nil {
+		return nil, err
+	}
+	usaRow.SetSelectable(false)
+	usaContainer, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	usaContainer.SetMarginStart(16)
+	usaContainer.SetMarginEnd(16)
+	usaContainer.SetMarginTop(12)
+	usaContainer.SetMarginBottom(12)
+	usaContainer.SetSpacing(12)
+	usaRow.Add(usaContainer)
+
+	usaCheck, err := gtk.CheckButtonNewWithLabel("")
+	if err != nil {
+		return nil, err
+	}
+	usaCheck.SetActive(true)
+	selectedRegionCheckboxes++
+	usaCheck.SetVAlign(gtk.ALIGN_CENTER)
 	usaCheck.Connect("toggled", func() {
 		if usaCheck.GetActive() {
 			selectedRegionCheckboxes++
@@ -103,12 +221,42 @@ func NewInitialSetupAssistantWindow(config *Config) (*InitialSetupAssistantWindo
 		}
 		assistant.SetPageComplete(page2, selectedRegionCheckboxes > 0)
 	})
-	regionBox.PackStart(usaCheck, true, true, 0)
+	usaContainer.PackStart(usaCheck, false, false, 0)
 
-	japanCheck, err := gtk.CheckButtonNewWithLabel("Japan")
+	usaLabel, err := gtk.LabelNew("")
 	if err != nil {
 		return nil, err
 	}
+	usaLabel.SetMarkup("<span font='12' weight='600'>USA</span>")
+	usaLabel.SetHAlign(gtk.ALIGN_START)
+	usaLabel.SetVAlign(gtk.ALIGN_CENTER)
+	usaContainer.PackStart(usaLabel, true, true, 0)
+	regionList.Add(usaRow)
+
+	// Japan
+	japanRow, err := gtk.ListBoxRowNew()
+	if err != nil {
+		return nil, err
+	}
+	japanRow.SetSelectable(false)
+	japanContainer, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	japanContainer.SetMarginStart(16)
+	japanContainer.SetMarginEnd(16)
+	japanContainer.SetMarginTop(12)
+	japanContainer.SetMarginBottom(12)
+	japanContainer.SetSpacing(12)
+	japanRow.Add(japanContainer)
+
+	japanCheck, err := gtk.CheckButtonNewWithLabel("")
+	if err != nil {
+		return nil, err
+	}
+	japanCheck.SetActive(true)
+	selectedRegionCheckboxes++
+	japanCheck.SetVAlign(gtk.ALIGN_CENTER)
 	japanCheck.Connect("toggled", func() {
 		if japanCheck.GetActive() {
 			selectedRegionCheckboxes++
@@ -117,67 +265,232 @@ func NewInitialSetupAssistantWindow(config *Config) (*InitialSetupAssistantWindo
 		}
 		assistant.SetPageComplete(page2, selectedRegionCheckboxes > 0)
 	})
-	regionBox.PackStart(japanCheck, true, true, 0)
+	japanContainer.PackStart(japanCheck, false, false, 0)
+
+	japanLabel, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	japanLabel.SetMarkup("<span font='12' weight='600'>Japan</span>")
+	japanLabel.SetHAlign(gtk.ALIGN_START)
+	japanLabel.SetVAlign(gtk.ALIGN_CENTER)
+	japanContainer.PackStart(japanLabel, true, true, 0)
+	regionList.Add(japanRow)
 
 	page3, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
 	if err != nil {
 		return nil, err
 	}
-	page3.SetBorderWidth(10)
+	page3.SetBorderWidth(24)
+	page3.SetSpacing(12)
 	assistant.AppendPage(page3)
 
-	page3Label, err := gtk.LabelNew("Please select your target platform(s):")
+	page3Label, err := gtk.LabelNew("")
 	if err != nil {
 		return nil, err
 	}
-	page3.PackStart(page3Label, true, true, 0)
+	page3Label.SetMarkup("<span font='14' weight='bold'>Where do you want to play your games?</span>")
+	page3Label.SetHAlign(gtk.ALIGN_START)
+	page3.PackStart(page3Label, false, false, 0)
 
-	cemuCheck, err := gtk.CheckButtonNewWithLabel("CEMU")
+	page3Desc, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	page3Desc.SetMarkup("<span font='11' alpha='80%'>Select one or both platforms. CEMU requires decryption, while Wii U keeps files encrypted for console use.</span>")
+	page3Desc.SetHAlign(gtk.ALIGN_START)
+	page3Desc.SetLineWrap(true)
+	page3.PackStart(page3Desc, false, false, 0)
+
+	platformList, err := gtk.ListBoxNew()
+	if err != nil {
+		return nil, err
+	}
+	platformList.SetSelectionMode(gtk.SELECTION_NONE)
+	platformList.SetActivateOnSingleClick(false)
+	page3.PackStart(platformList, true, true, 8)
+
+	// CEMU
+	cemuRow, err := gtk.ListBoxRowNew()
+	if err != nil {
+		return nil, err
+	}
+	cemuRow.SetSelectable(false)
+	cemuOuterContainer, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	cemuOuterContainer.SetMarginStart(16)
+	cemuOuterContainer.SetMarginEnd(16)
+	cemuOuterContainer.SetMarginTop(12)
+	cemuOuterContainer.SetMarginBottom(12)
+	cemuOuterContainer.SetSpacing(12)
+	cemuRow.Add(cemuOuterContainer)
+
+	cemuCheck, err := gtk.CheckButtonNewWithLabel("")
 	if err != nil {
 		return nil, err
 	}
 	cemuCheck.SetActive(true)
+	cemuCheck.SetVAlign(gtk.ALIGN_START)
+	cemuOuterContainer.PackStart(cemuCheck, false, false, 0)
 
-	wiiUCheck, err := gtk.CheckButtonNewWithLabel("Wii U")
+	cemuTextBox, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	cemuTextBox.SetSpacing(2)
+
+	cemuMainLabel, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	cemuMainLabel.SetMarkup("<span font='12' weight='600'>CEMU - Emulator</span>")
+	cemuMainLabel.SetHAlign(gtk.ALIGN_START)
+	cemuTextBox.PackStart(cemuMainLabel, false, false, 0)
+
+	cemuSubLabel, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	cemuSubLabel.SetMarkup("<span font='10' alpha='80%'>Decrypt game files for use in the CEMU emulator</span>")
+	cemuSubLabel.SetLineWrap(true)
+	cemuSubLabel.SetHAlign(gtk.ALIGN_START)
+	cemuTextBox.PackStart(cemuSubLabel, false, false, 0)
+
+	cemuOuterContainer.PackStart(cemuTextBox, true, true, 0)
+	platformList.Add(cemuRow)
+
+	// Wii U
+	wiiURow, err := gtk.ListBoxRowNew()
+	if err != nil {
+		return nil, err
+	}
+	wiiURow.SetSelectable(false)
+	wiiUOuterContainer, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	wiiUOuterContainer.SetMarginStart(16)
+	wiiUOuterContainer.SetMarginEnd(16)
+	wiiUOuterContainer.SetMarginTop(12)
+	wiiUOuterContainer.SetMarginBottom(12)
+	wiiUOuterContainer.SetSpacing(12)
+	wiiURow.Add(wiiUOuterContainer)
+
+	wiiUCheck, err := gtk.CheckButtonNewWithLabel("")
 	if err != nil {
 		return nil, err
 	}
 	wiiUCheck.SetActive(true)
+	wiiUCheck.SetVAlign(gtk.ALIGN_START)
+	wiiUOuterContainer.PackStart(wiiUCheck, false, false, 0)
 
-	platformBox, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
+	wiiUTextBox, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
 	if err != nil {
 		return nil, err
 	}
-	page3.PackStart(platformBox, true, true, 0)
-	platformBox.PackStart(cemuCheck, true, true, 0)
-	platformBox.PackStart(wiiUCheck, true, true, 0)
+	wiiUTextBox.SetSpacing(2)
+
+	wiiUMainLabel, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	wiiUMainLabel.SetMarkup("<span font='12' weight='600'>Wii U Console</span>")
+	wiiUMainLabel.SetHAlign(gtk.ALIGN_START)
+	wiiUTextBox.PackStart(wiiUMainLabel, false, false, 0)
+
+	wiiUSubLabel, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	wiiUSubLabel.SetMarkup("<span font='10' alpha='80%'>Keep encrypted game files for installation on a Wii U console</span>")
+	wiiUSubLabel.SetLineWrap(true)
+	wiiUSubLabel.SetHAlign(gtk.ALIGN_START)
+	wiiUTextBox.PackStart(wiiUSubLabel, false, false, 0)
+
+	wiiUOuterContainer.PackStart(wiiUTextBox, true, true, 0)
+	platformList.Add(wiiURow)
 
 	page4, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0) // Finish page
 	if err != nil {
 		return nil, err
 	}
-	page4.SetBorderWidth(10)
+	page4.SetBorderWidth(24)
+	page4.SetSpacing(16)
 	assistant.AppendPage(page4)
 
-	page4Label, err := gtk.LabelNew("You have successfully set up WiiUDownloader!\nYou can always change these settings from the program interface itself.\nClick Apply to finish.")
+	page4Label, err := gtk.LabelNew("")
 	if err != nil {
 		return nil, err
 	}
-	page4.PackStart(page4Label, true, true, 0)
+	page4Label.SetMarkup("<span font='18' weight='bold'>All Set!</span>")
+	page4Label.SetHAlign(gtk.ALIGN_START)
+	page4.PackStart(page4Label, false, false, 0)
+
+	page4SubLabel, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	page4SubLabel.SetMarkup("<span font='11' alpha='80%'>WiiUDownloader is now configured and ready to use. You can start downloading games immediately or adjust settings in the preferences menu.</span>")
+	page4SubLabel.SetLineWrap(true)
+	page4SubLabel.SetHAlign(gtk.ALIGN_START)
+	page4.PackStart(page4SubLabel, false, false, 0)
+
+	// Spacing
+	spacer4, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	page4.PackStart(spacer4, true, true, 0)
+
+	// Summary box
+	summaryLabel, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	summaryLabel.SetMarkup("<span font='10' weight='600'>Configuration Summary:</span>")
+	summaryLabel.SetHAlign(gtk.ALIGN_START)
+	page4.PackStart(summaryLabel, false, false, 0)
+
+	summaryBox, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	summaryBox.SetSpacing(4)
+	summaryBox.SetMarginTop(8)
+	summaryBox.SetMarginStart(8)
+	page4.PackStart(summaryBox, false, false, 0)
+
+	summaryRegions, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	summaryRegions.SetMarkup("<span font='10' alpha='85%'>✓ Regions: Europe, USA, Japan</span>")
+	summaryRegions.SetHAlign(gtk.ALIGN_START)
+	summaryBox.PackStart(summaryRegions, false, false, 0)
+
+	summaryPlatforms, err := gtk.LabelNew("")
+	if err != nil {
+		return nil, err
+	}
+	summaryPlatforms.SetMarkup("<span font='10' alpha='85%'>✓ Platforms: CEMU + Wii U</span>")
+	summaryPlatforms.SetHAlign(gtk.ALIGN_START)
+	summaryBox.PackStart(summaryPlatforms, false, false, 0)
 
 	assistant.SetPageComplete(page1, true)
-	assistant.SetPageComplete(page2, false)
-	assistant.SetPageComplete(page3, true) // TODO: Check if at least one platform is selected
+	assistant.SetPageComplete(page2, true)
+	assistant.SetPageComplete(page3, true)
 	assistant.SetPageComplete(page4, true)
 
 	assistant.SetPageType(page1, gtk.ASSISTANT_PAGE_INTRO)
-	assistant.SetPageType(page2, gtk.ASSISTANT_PAGE_PROGRESS)
-	assistant.SetPageType(page3, gtk.ASSISTANT_PAGE_PROGRESS)
+	assistant.SetPageType(page2, gtk.ASSISTANT_PAGE_CONTENT)
+	assistant.SetPageType(page3, gtk.ASSISTANT_PAGE_CONTENT)
 	assistant.SetPageType(page4, gtk.ASSISTANT_PAGE_CONFIRM)
 
 	assistant.SetPageTitle(page1, "Welcome")
-	assistant.SetPageTitle(page2, "Region")
-	assistant.SetPageTitle(page3, "Platform")
+	assistant.SetPageTitle(page2, "Regions")
+	assistant.SetPageTitle(page3, "Platforms")
 	assistant.SetPageTitle(page4, "Finish")
 
 	assistant.Connect("apply", func() {
